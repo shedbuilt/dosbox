6dca1e7b9117c045a8df3a37e9d620725c2cf74e
Auston Stewart <auston@shedbuilt.net>
2018-10-24 20:29:36 -1000

    Updating to SVN r4168

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-# Disable keyboard scancodes by default to prevent input issues
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+# Patch
 patch -Np1 -i "${SHED_PKG_PATCH_DIR}/dosbox_svn_r4088_disable_usescancodes.patch" &&
-./autogen.sh &&
-./configure --prefix=/usr/local \
-            --disable-opengl &&
+./autogen.sh || exit 1
+# Configure
+if [ -z "${SHED_PKG_LOCAL_OPTIONS[gl]}" ]; then
+    ./configure --prefix=/usr/local \
+                --disable-opengl || exit 1
+else
+    ./configure --prefix=/usr/local || exit 1
+fi
+# Build and Install
 make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install
